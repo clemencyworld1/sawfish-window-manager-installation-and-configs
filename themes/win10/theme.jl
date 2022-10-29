a3e69c01a4dad25e71c538541b7f83e5edacf1f6
Okeke Uchechukwu Callistus <42145055+clemencyworld1@users.noreply.github.com>
2022-10-29 14:15:50 +0100

    Add files via upload

--- themes/win10/theme.jl
+++ themes/win10/theme.jl
@@ -15,13 +15,13 @@
 
     (default-frame
      `(
-        ((background . ,ititle)  (right-edge . -4) (left-edge . -4)              (height . 40) (top-edge . -40) 
+        ((background . ,ititle)  (right-edge . 106) (left-edge . 36)              (height . 40) (top-edge . -40) 
          (foreground . ("grey" "black"))  (text . ,window-name) (x-justify . center) (y-justify . center) (class . title))
 
-        ((background . ,iclose)  (right-edge . 1)        (height . 40)           (width . 40)               (top-edge . -40)                    (class . close-button))
-        ((background . ,imax)    (right-edge . 35)       (height . 40)           (width . 40)               (top-edge . -40)                    (class . maximize-button))
-        ((background . ,imin)    (right-edge . 70)         (height . 40)         (width . 40)               (top-edge . -40)                    (class . iconify-button))
-        ((background . ,imenu)                     (left-edge . 0)  (width . 40)    (height . 40)           (top-edge . -40)                    (class . menu-button))
+        ((background . ,iclose)  (right-edge . -4)        (height . 40)           (width . 40)               (top-edge . -40)                    (class . close-button))
+        ((background . ,imax)    (right-edge . 31)       (height . 40)           (width . 40)               (top-edge . -40)                    (class . maximize-button))
+        ((background . ,imin)    (right-edge . 66)         (height . 40)         (width . 40)               (top-edge . -40)                    (class . iconify-button))
+        ((background . ,imenu)                     (left-edge . -4)  (width . 40)    (height . 40)           (top-edge . -40)                    (class . menu-button))
         ((background . ,iright)  (right-edge . -4)                  (width . 4)                (top-edge . 0)   (bottom-edge . 0)  (class . right-border))
         ((background . ,ileft)                     (left-edge . -4) (width . 4)                (top-edge . 0)   (bottom-edge . 0)  (class . left-border))
         ((background . ,ibottom) (right-edge . -4)  (left-edge . -4)  (height . 4)               (bottom-edge . -4) (class . bottom-border))
@@ -31,45 +31,42 @@
 
     (shaded-frame
      `(
-        ((background . ,ititle)  (right-edge . -4) (left-edge . -4)              (height . 40) (top-edge . -40) 
+        ((background . ,ititle)  (right-edge . 106) (left-edge . 36)              (height . 40) (top-edge . -40) 
          (foreground . ("grey" "black"))  (text . ,window-name) (x-justify . center) (y-justify . center) (class . title))
 
-        ((background . ,iclose)  (right-edge . 1)        (height . 40)           (width . 40)               (top-edge . -40)                    (class . close-button))
-        ((background . ,imax)    (right-edge . 35)       (height . 40)           (width . 40)               (top-edge . -40)                    (class . maximize-button))
-        ((background . ,imin)    (right-edge . 70)         (height . 40)         (width . 40)               (top-edge . -40)                    (class . iconify-button))
+        ((background . ,iclose)  (right-edge . -4)        (height . 40)           (width . 40)               (top-edge . -40)                    (class . close-button))
+        ((background . ,imax)    (right-edge . 31)       (height . 40)           (width . 40)               (top-edge . -40)                    (class . maximize-button))
+        ((background . ,imin)    (right-edge . 66)         (height . 40)         (width . 40)               (top-edge . -40)                    (class . iconify-button))
         ((background . ,imenu)                     (left-edge . 0)  (width . 40)    (height . 40)           (top-edge . -40)                    (class . menu-button))
-        ((background . ,ibottom) (right-edge . 0)  (left-edge . 0)  (height . 4)               (top-edge . 0) (class . bottom-border))
-
-
+        ((background . ,ibottom) (right-edge . -4)  (left-edge . 0)  (height . 4)               (top-edge . 0) (class . bottom-border))
       )
     )
 
     (transident-frame
      `(
-        ((background . ,ititle)  (right-edge . -4) (left-edge . -4)              (height . 40) (top-edge . -40) 
+        ((background . ,ititle)  (right-edge . 106) (left-edge . 36)              (height . 40) (top-edge . -40) 
          (foreground . ("grey" "black"))  (text . ,window-name) (x-justify . center) (y-justify . center) (class . title))
 
-        ((background . ,iclose)  (right-edge . 1)        (height . 40)           (width . 40)               (top-edge . -40)                    (class . close-button))
-        ((background . ,imax)    (right-edge . 35)       (height . 40)           (width . 40)               (top-edge . -40)                    (class . maximize-button))
-        ((background . ,imin)    (right-edge . 70)         (height . 40)         (width . 40)               (top-edge . -40)                    (class . iconify-button))
-        ((background . ,imenu)                     (left-edge . 0)  (width . 40)    (height . 40)           (top-edge . -40)                    (class . menu-button))
+        ((background . ,iclose)  (right-edge . -4)        (height . 40)           (width . 40)               (top-edge . -40)                    (class . close-button))
+        ((background . ,imax)    (right-edge . 31)       (height . 40)           (width . 40)               (top-edge . -40)                    (class . maximize-button))
+        ((background . ,imin)    (right-edge . 66)         (height . 40)         (width . 40)               (top-edge . -40)                    (class . iconify-button))
+        ((background . ,imenu)                     (left-edge . -4)  (width . 40)    (height . 40)           (top-edge . -40)                    (class . menu-button))
         ((background . ,iright)  (right-edge . -4)                  (width . 4)                (top-edge . 0)   (bottom-edge . 0)  (class . right-border))
         ((background . ,ileft)                     (left-edge . -4) (width . 4)                (top-edge . 0)   (bottom-edge . 0)  (class . left-border))
         ((background . ,ibottom) (right-edge . -4)  (left-edge . -4)  (height . 4)               (bottom-edge . -4) (class . bottom-border))
-
       )
     )
 
     (shaded-transident-frame
      `(
-        ((background . ,ititle)  (right-edge . -4) (left-edge . -4)              (height . 40) (top-edge . -40) 
+        ((background . ,ititle)  (right-edge . 106) (left-edge . 36)              (height . 40) (top-edge . -40) 
          (foreground . ("grey" "black"))  (text . ,window-name) (x-justify . center) (y-justify . center) (class . title))
 
-        ((background . ,iclose)  (right-edge . 1)        (height . 40)           (width . 40)               (top-edge . -40)                    (class . close-button))
-        ((background . ,imax)    (right-edge . 35)       (height . 40)           (width . 40)               (top-edge . -40)                    (class . maximize-button))
-        ((background . ,imin)    (right-edge . 70)         (height . 40)         (width . 40)               (top-edge . -40)                    (class . iconify-button))
+        ((background . ,iclose)  (right-edge . -4)        (height . 40)           (width . 40)               (top-edge . -40)                    (class . close-button))
+        ((background . ,imax)    (right-edge . 31)       (height . 40)           (width . 40)               (top-edge . -40)                    (class . maximize-button))
+        ((background . ,imin)    (right-edge . 66)         (height . 40)         (width . 40)               (top-edge . -40)                    (class . iconify-button))
         ((background . ,imenu)                     (left-edge . 0)  (width . 40)    (height . 40)           (top-edge . -40)                    (class . menu-button))
-        ((background . ,ibottom) (right-edge . 0)  (left-edge . 0)  (height . 4)               (top-edge . 0) (class . bottom-border))
+        ((background . ,ibottom) (right-edge . -4)  (left-edge . 0)  (height . 4)               (top-edge . 0) (class . bottom-border))
 
       )
     )
